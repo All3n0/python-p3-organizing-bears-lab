b2bd71b22514657c3f63cffddfddfb2eba8e30ff
ALLAN <allankipr6@gmail.com>
2024-05-31 20:00:36 +0300

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Mr. Chocolate",12,"M","Red","jiberish",true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Rowdy",16,"M","Red","jiberish",true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Tabitha",14,"F","Red","jiberish",true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Sergeant Brown",13,"M","Red","jiberish",true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Melissa",19,"F","Red","jiberish",true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Grinch",12,"M","Red","jiberish",true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES("Wendy",11,"F","Red","jiberish",true);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES(NULL,12,"M","Red","jiberish",true);
